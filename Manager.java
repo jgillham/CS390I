8e82ea4c283215d6f8549e98f40d51ed0ed28808
Josh Gillham <usajoshgillham@gmail.com>
2012-09-17 13:31:43 -0600

    Initialized.

---
+++ Manager.java
@@ -0,0 +1,16 @@
+
+/**
+ * Manages the Player class and keeps the information seperate from the GUI
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Manager
+{
+    /**
+     * Creates a new player in the Manager class and adds him to the list.
+     * 
+     * @return the id of the new player
+     */
+    public int newPlayer();
+}
