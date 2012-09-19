472a6d59e77c2f0f30fef43ff584ea8084e0a970
Josh Gillham <usajoshgillham@gmail.com>
2012-09-18 22:11:19 -0600

    Updated manager class with addtional documentation and method stubs.

--- Manager.java
+++ Manager.java
@@ -8,22 +8,68 @@
  * 
  * Private Data:
  *  Players - a linkedlist of players
+ *  Score - number of successful guesses
+ *  Player up - the player who has the turn to guess
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Josh Gillham
+ * @version 9-18-12
  */
 public interface Manager
 {
     /**
      * Creates a new player in the Manager class and adds him to the list.
      * 
-     * @return the id of the new player
+     * @return the id of the new player.
+     * 
+     * @throws an IllegalArgumentException when the player's name is invalid.
+     * @throws an NullPointerException when the player's name is null.
      */
-    public int newPlayer();
+    public Player addPlayer( String name );
     /**
      * The manager can kick a player off the list.\
      * 
-     * @return true if the kick was successful
+     * Preconditions:
+     *  addPlayer must have been successfully called at least once.
+     * 
+     * @return true if the kick was successful or false if not.
+     * 
+     * @throws an NullPointerException when player is null
      */
-    public boolean kickPlayer( int playerID );
+    public boolean kickPlayer( Player player );
+    
+    /**
+     * Gets the score of the team.
+     * 
+     * @return the score.
+     */
+    public int getScore();
+    
+    /**
+     * Gets the players whose turn it is to guess.
+     * 
+     * Preconditions:
+     *  addPlayer must have been successfully called at least once.
+     * 
+     * @return the player.
+     * 
+     * @throws NoSuchElementException when getRosterSize() == 0.
+     */
+    public Player getPlayerUp();
+    
+    /**
+     * Gets the roster size with List.size().
+     * 
+     * @return the number of players on the team.
+     */
+    public int getRosterSize();
+    
+    /**
+     * Sets the player up to the next player in the line up.
+     * 
+     * Preconditions:
+     *  addPlayer must have been successfully called at least once.
+     * 
+     * @throws NoSuchElementException when getRosterSize() == 0.
+     */
+    public void nextPlayer();
 }
