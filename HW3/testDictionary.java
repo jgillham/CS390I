db82b9bcfc48f5691de59769ce7740a3840c5bd6
Josh Gillham <usajoshgillham@gmail.com>
2012-09-19 20:52:56 -0600

    Added empty test units for Game, Player, and Dictionary.

---
+++ HW3/testDictionary.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * This unit test will check the integrity of the Dictionary class.
+ *
+ * @author  Josh Gillham
+ * @version 9-19-12
+ */
+public class testDictionary
+{
+    /**
+     * Default constructor for test class testDictionary
+     */
+    public testDictionary()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
---
+++ HW3/testGame.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * This test unit will check the integrity of the Game class.
+ *
+ * @author  Josh Gillham
+ * @version 9-19-12
+ */
+public class testGame
+{
+    /**
+     * Default constructor for test class testGame
+     */
+    public testGame()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
