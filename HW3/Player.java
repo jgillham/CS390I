e65de0fbc3e56a07c7113fde8b16ed5a9fded739
Josh Gillham <usajoshgillham@gmail.com>
2012-09-19 20:46:43 -0600

    Added empty bodies for Player.java.

---
+++ HW3/Player.java
@@ -0,0 +1,19 @@
+
+/**
+ * This currently contains only empty bodies for test integrity.
+ * 
+ * @author Josh Gillham
+ * @version 9-19-12
+ */
+public class Player implements PlayerInterface {
+    /**
+     * Adds the letter to the list of guesses. If goodGuess is false, then 
+     *  increments the wrong answers counter by 1.
+     * 
+     * @arg letter the guess
+     * @arg goodGuess true if the letter is found in the goal word
+     */
+    public void recordGuess( char letter, boolean goodGuess ) {
+        
+    }
+}
