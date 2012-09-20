3b259cdcee3d8b4c6b270b8e5d6efb8e56b9ab23
Josh Gillham <usajoshgillham@gmail.com>
2012-09-19 20:43:33 -0600

    Added empty bodies for Dictionary.java.

---
+++ HW3/Dictionary.java
@@ -0,0 +1,31 @@
+ 
+/**
+ * Currently this will have only empty bodies for test itegrity.
+ * 
+ * @author Josh Gillham
+ * @version 9-19-12
+ */
+public class Dictionary implements DictionaryInterface {
+    /**
+     * getInstance finds the instance of the singleton and the file is disregarded, but, if
+     *  no instance exists, a new dictionary class is created and the file is loaded.
+     * 
+     * @arg file the dictionary file to load.
+     * 
+     * @return the Dictionary instance
+     */
+    public DictionaryInterface getInstance( String file ) {
+        return null;
+    }
+    
+    /**
+     * Finds a random word from the dictionary.
+     * 
+     * @arg length is the length of the word to find
+     * 
+     * @return the random word
+     */
+    public String getWord( int length ) {
+        return null;
+    }
+}
