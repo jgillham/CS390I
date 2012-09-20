01589e473af37b10d64561979320b9fe4ecd6b53
Josh Gillham <usajoshgillham@gmail.com>
2012-09-20 02:07:26 -0600

    Added constants to Dictionary.java.

--- HW3/Dictionary.java
+++ HW3/Dictionary.java
@@ -6,6 +6,8 @@
  * @version 9-19-12
  */
 public class Dictionary implements DictionaryInterface {
+    static public final int MIN_WORDLENGTH= 3;
+    static public final int MAX_WORDLENGTH= 7;
     /**
      * getInstance finds the instance of the singleton and the file is disregarded, but, if
      *  no instance exists, a new dictionary class is created and the file is loaded.
