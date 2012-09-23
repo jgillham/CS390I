d761188094499b667ce688dec609279fb82b4d42
Josh Gillham <usajoshgillham@gmail.com>
2012-09-23 02:16:47 -0600

    Added new test units testGameUI and testSetupUI.

--- HW3/SetupUI.java
+++ HW3/SetupUI.java
@@ -5,7 +5,7 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Hangman{
+public class SetupUI{
     static public void main( String[] args )
     { throw new UnsupportedOperationException(); }
 }
