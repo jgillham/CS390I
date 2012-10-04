0d935462b0108d5a649dc5f91ac545dc5e28d82f
Josh Gillham <usajoshgillham@gmail.com>
2012-10-04 14:00:21 -0600

    Wrote more tests in integrationTestGamePlay.
    There is a bug where a illegal guess is not detected.

--- HW3/integrationTestGamePlay.java
+++ HW3/integrationTestGamePlay.java
@@ -52,24 +52,136 @@ public class integrationTestGamePlay {
 
     
     
-    
+    final int MAX_TURNS= 20;
     @Test
     public void testPlayerWins(){
+        final int maxGuesses= gameWord.length();
         GameEventsBaseTester tester= new GameEventsBaseTester(){
+            int badGuesses= 0;
+            int errorGuesses= 0;
             int i= 0;
             public void playerUp( Player player ){
-                game.makeGuess( gameWord.charAt( i++ ) );
+                if( i < maxGuesses )try {
+                    if( !game.makeGuess( gameWord.charAt( i++ ) ) )
+                        ++badGuesses;
+                } catch( Exception e ) {
+                    ++errorGuesses;
+                }
+            }
+            public void makeAssertions(){
+                assertFalse( gameOver );
+                assertNotNull( gameWinningTeam );
+                assertEquals( 0, badGuesses );
+                assertEquals( 0, errorGuesses );
+            }
+        };
+        game.setMaxAttempts( maxGuesses );
+        game.setGameEventsHandler( tester );
+        
+        int counter= 0;
+        while( counter++ < MAX_TURNS && game.getGameState() == Logic.Statis.STARTED ) {
+            game.rotateTurn();
+        }
+        tester.makeAssertions();
+    }
+    
+    @Test
+    public void testPlayerLooses(){
+        final String guesses= "abcdefghijkl";
+        final int maxGuesses= gameWord.length();
+        GameEventsBaseTester tester= new GameEventsBaseTester(){
+            int badGuesses= 0;
+            int errorGuesses= 0;
+            
+            int i= 0;
+            public void playerUp( Player player ){
+                if( i < maxGuesses )try {
+                    if( !game.makeGuess( guesses.charAt( i++ ) ) )
+                        ++badGuesses;
+                } catch( Exception e ) {
+                    ++errorGuesses;
+                }
             }
             public void makeAssertions(){
                 assertTrue( gameOver );
-                assertNotNull( gameWinningTeam );
+                assertNull( gameWinningTeam );
+                assertEquals( maxGuesses, badGuesses );
+                assertEquals( 0, errorGuesses );
             }
         };
+        game.setMaxAttempts( maxGuesses );
         game.setGameEventsHandler( tester );
-        
-        while( game.getGameState() == Logic.Statis.STARTED ) {
+        int counter= 0;
+        while( counter++ < MAX_TURNS && game.getGameState() == Logic.Statis.STARTED ) {
             game.rotateTurn();
         }
+        assertTrue( counter < MAX_TURNS );
+        tester.makeAssertions();
+    }
+    @Test
+    public void testPlayerHasAFewBadGuessesButWins(){
+        final String guesses= "fyu.eyl";
+        final int maxGuesses= gameWord.length() + 3;
+        GameEventsBaseTester tester= new GameEventsBaseTester(){
+            int badGuesses= 0;
+            int errorGuesses= 0;
+            
+            int i= 0;
+            public void playerUp( Player player ){
+                if( i < maxGuesses ) try {
+                    if( !game.makeGuess( guesses.charAt( i++ ) ) )
+                        ++badGuesses;
+                } catch( Exception e ) {
+                    ++errorGuesses;
+                }
+            }
+            public void makeAssertions(){
+                assertFalse( gameOver );
+                assertNotNull( gameWinningTeam );
+                assertEquals( 1, badGuesses );
+                assertEquals( 2, errorGuesses );
+            }
+        };
+        game.setMaxAttempts( maxGuesses );
+        game.setGameEventsHandler( tester );
+        int counter= 0;
+        while( counter++ < MAX_TURNS && game.getGameState() == Logic.Statis.STARTED ) {
+            game.rotateTurn();
+        }
+        assertTrue( counter < MAX_TURNS );
+        tester.makeAssertions();
+    }
+    @Test
+    public void testPlayerHasAFewBadGuessesButLooses(){
+        final String guesses= "fyu.eyl";
+        final int maxGuesses= gameWord.length() + 2;
+        GameEventsBaseTester tester= new GameEventsBaseTester(){
+            int badGuesses= 0;
+            int errorGuesses= 0;
+            
+            int i= 0;
+            public void playerUp( Player player ){
+                if( i < maxGuesses ) try {
+                    if( !game.makeGuess( guesses.charAt( i++ ) ) )
+                        ++badGuesses;
+                } catch( Exception e ) {
+                    ++errorGuesses;
+                }
+            }
+            public void makeAssertions(){
+                assertFalse( gameOver );
+                assertNotNull( gameWinningTeam );
+                assertEquals( 1, badGuesses );
+                assertEquals( 2, errorGuesses );
+            }
+        };
+        game.setMaxAttempts( maxGuesses );
+        game.setGameEventsHandler( tester );
+        int counter= 0;
+        while( counter++ < MAX_TURNS && game.getGameState() == Logic.Statis.STARTED ) {
+            game.rotateTurn();
+        }
+        assertTrue( counter < MAX_TURNS );
         tester.makeAssertions();
     }
 }
