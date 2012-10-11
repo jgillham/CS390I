ca1e3aa8ab1fd88bc63df697e0d944e8e73beffb
Josh Gillham <usajoshgillham@gmail.com>
2012-10-10 18:54:39 -0600

    Created unit test for WordCanidates.

---
+++ HW4/testWordCanidates.java
@@ -0,0 +1,16 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class testWordCanidates.
+ *
+ * @author  Josh Gillham
+ * @version 10-10-12
+ */
+public class testWordCanidates {
+    
+}
