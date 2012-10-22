c32ae60626c045dd8eda2e2f55d39eb5d85d2eec
Josh Gillham <usajoshgillham@gmail.com>
2012-10-21 21:10:54 -0600

    Behavioral tests.
    Wrote in QuestionNodeTest, ThingNodeTest, abd testBTNode.

--- HW5/testBTNode.java
+++ HW5/testBTNode.java
@@ -6,37 +6,116 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * The test class testBTNode.
+ * Tests each method to prove that behavior is consistant throughout the class.
  *
- * @author  (your name)
- * @version (a version number or a date)
+ * @author  Josh Gillham
+ * @version 10-21-12
  */
-public class testBTNode
-{
+public class testBTNode {
     /**
-     * Default constructor for test class testBTNode
+     * Test default constructor. All private fields should be null.
+     * 
+     * Proves the constructor is setting private fields properly.
      */
-    public testBTNode()
-    {
+    @Test
+    public void testConstructor(){
+        BTNode instance= new BTNode();
+        assertNull( instance.getLeftChild() );
+        assertNull( instance.getRightChild() );
+        assertNull( instance.getValue() );
     }
-
+    
     /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
+     * Test constructor with 1 parameter. getValue() should be the same as the parameter.
+     *  Other fields will be null.
+     *  
+     * Proves the constructor is setting private fields properly.
      */
-    @Before
-    public void setUp()
-    {
+    @Test
+    public void testConstructor_1param(){
+        String expectedValue= "test";
+        BTNode< String > instance= new BTNode< String >( expectedValue);
+        assertNull( instance.getLeftChild() );
+        assertNull( instance.getRightChild() );
+        assertEquals( expectedValue, instance.getValue() );
     }
-
+    
     /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
+     * Test constructor with 3 parameter. Get methods should return the parameters.
+     * 
+     * Proves the constructor is setting private fields properly.
      */
-    @After
-    public void tearDown()
-    {
+    @Test
+    public void testConstructor_3param(){
+        String expectedValue= "test";
+        BTNode< String > expectedRightNode= new BTNode< String >();
+        BTNode< String > expectedLeftNode= new BTNode< String >();
+        BTNode< String > instance= new BTNode< String >( 
+            expectedValue, expectedLeftNode, expectedRightNode
+        );
+        assertEquals( expectedLeftNode, instance.getLeftChild() );
+        assertEquals( expectedRightNode, instance.getRightChild() );
+        assertEquals( expectedValue, instance.getValue() );
+    }
+    
+    /**
+     * Proves that the set function affects what the get function returns.
+     */
+    @Test
+    public void testSetAndGetValue() {
+        String expected= "test";
+        BTNode< String > instance= new BTNode< String >();
+        instance.setValue( expected );
+        assertEquals( expected, instance.getValue() );
+        expected= "apple";
+        instance.setValue( expected );
+        assertEquals( expected, instance.getValue() );
+        expected= "orange";
+        instance.setValue( expected );
+        assertEquals( expected, instance.getValue() );
+    }
+    
+    /**
+     * Proves set function affects what the get function returns.
+     */
+    @Test
+    public void testSetAndGetLeftChild() {
+        BTNode< String > expected= new BTNode< String >();
+        BTNode< String > instance= new BTNode< String >();
+        instance.setLeftChild( expected );
+        assertEquals( expected, instance.getLeftChild() );
+        expected= new BTNode< String >();
+        instance.setLeftChild( expected );
+        assertEquals( expected, instance.getLeftChild() );
+        expected= new BTNode< String >();
+        instance.setLeftChild( expected );
+        assertEquals( expected, instance.getLeftChild() );
+    }
+    
+    /**
+     * Proves set function affects what the get function returns.
+     */
+    @Test
+    public void testSetAndGetRightChild() {
+        BTNode< String > expected= new BTNode< String >();
+        BTNode< String > instance= new BTNode< String >();
+        instance.setRightChild( expected );
+        assertEquals( expected, instance.getRightChild() );
+        expected= new BTNode< String >();
+        instance.setRightChild( expected );
+        assertEquals( expected, instance.getRightChild() );
+        expected= new BTNode< String >();
+        instance.setRightChild( expected );
+        assertEquals( expected, instance.getRightChild() );
+    }
+    
+    /**
+     * Proves toString ...
+     * 
+     * TODO
+     */
+    @Test
+    public void testToString() {
+        throw new UnsupportedOperationException();
     }
 }
