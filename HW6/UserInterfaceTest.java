33602d18586c5581b4ee7a0fbc9c6227057a7462
Josh Gillham <usajoshgillham@gmail.com>
2012-11-01 23:33:36 -0600

    Wrote tests for getCharacteristics.

--- HW6/UserInterfaceTest.java
+++ HW6/UserInterfaceTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Iterator;
 
 /**
  * The test class UserInterfaceTest.
@@ -35,15 +37,60 @@ public class UserInterfaceTest {
     
     @Test
     public void testGetChoices() {
-        List< Choices > expected = new List< Choices >();
+        List< String > expected = new LinkedList< String >();
+        expected.add( "hp" );
+        expected.add( "dell" );
+        expected.add( "sony" );
+        InstrumentationUI instance = new InstrumentationUI();
+        for( String choice: expected ) {
+            instance.answers.add( choice );
+        }
+        List< Choice > actualChoices = instance.getChoices();
+        Iterator< String > expI = expected.iterator();
+        for( Choice actualChoice: actualChoices ) {
+            assertTrue( expI.hasNext() );
+            assertEquals( expI.next(), actualChoice );
+        }
+    }
+    
+    @Test
+    public void testGetCharacteristics() {
+        List< String > expected = new LinkedList< String >();
         expected.add( "color" );
         expected.add( "price" );
         expected.add( "speed" );
         InstrumentationUI instance = new InstrumentationUI();
         for( String choice: expected ) {
-            instance.add( choice );
+            instance.answers.add( choice );
         }
-        List< Choice > actual = instance.getChoices();
-        assertEquals( expected, actual );
+        List< Choice > actualChoices = instance.getChoices();
+        Iterator< String > expI = expected.iterator();
+        for( Choice actualChoice: actualChoices ) {
+            assertTrue( expI.hasNext() );
+            assertEquals( expI.next(), actualChoice );
+        }
+    }
+    
+    
+    
+    @Test
+    public void testGetCharacteristicRankings() {
+        int[] expected = {
+            1, 2, 3, 4, 5, 6  
+        };
+        InstrumentationUI instance = new InstrumentationUI();
+        for( int val: expected ) {
+            instance.answers.add( Integer.toString( val ) );
+        }
+        List< Characteristic > chars = new LinkedList< Characteristic >();
+        chars.add( new Characteristic( "color" ) );
+        chars.add( new Characteristic( "price" ) );
+        chars.add( new Characteristic( "speed" ) );
+        instance.getCharacteristicRankings( chars, 1 );
+        int i = 0;
+        for( Characteristic chr: chars ) {
+            assertTrue( i < expected.length );
+            assertEquals( expected[ i ], chr.getRank() );
+        }
     }
 }
