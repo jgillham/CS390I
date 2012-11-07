7c2a9b5f4836992b55e9e3ccebb8dde39e5e755b
Josh Gillham <usajoshgillham@gmail.com>
2012-11-07 15:38:04 -0700

    Wrote tests for new methods.

--- HW6/DeciderTest.java
+++ HW6/DeciderTest.java
@@ -80,4 +80,55 @@ public class DeciderTest {
         }
     }
     
+    @Test
+    public void testCalculateUnnormalizedFinalScores() {
+        List< Characteristic > chars = new LinkedList< Characteristic >();
+        Characteristic chr = new Characteristic( "color" );
+        chr.setRank( 1 );
+        chars.add( chr );
+        chr = new Characteristic( "price" );
+        chr.setRank( 2 );
+        chars.add( chr );
+        List< Choice > choices = new LinkedList< Choice >();
+        choices.add( new Choice( "hp" ) );
+        choices.add( new Choice( "dell" ) );
+        double[][] crossRankings = { 
+            { 0.25D, 0.25D },
+            { 0.75D, 0.75D }
+        };
+        double[] expectedFinalScore = {
+            1D * 0.25D + 0.25D * 2D,
+            1D * 0.75D + 0.75D * 2D
+        };
+        double[] actual = Decider.calculateUnnormalizedFinalScores( choices, chars, crossRankings );
+        assertEquals( expectedFinalScore.length, choices.size() );
+        assertEquals( expectedFinalScore.length, actual.length );
+        for( int i = 0; i < choices.size(); ++i ) {
+            assertEquals( expectedFinalScore[ i ], actual[ i ], 0.001 );
+        }
+    }
+    @Test
+    public void testFindMax() {
+        double[] data = {
+            1, 2, 3, 2.9, 1.1
+        };
+        assertEquals( 3, Decider.findMax( data ), 0.0001 );
+    }
+    
+    @Test
+    public void testNormalizeValues() {
+        double[] data = {
+            1, 2, 3, 2.9, 1.1
+        };
+        int[] expected = {
+            33, 67,  100, 97, 37
+        };
+        int[] actual = Decider.normalizeValues( data, 3D, 100D );
+        assertEquals( expected.length, actual.length );
+        for( int i = 0; i < expected.length; ++i ) {
+            assertEquals( expected[ i ], actual[ i ] );
+        }
+        
+    }
+    
 }
