a96a6e97374e2a253306dc7b828419f9d7af63d6
Josh Gillham <usajoshgillham@gmail.com>
2012-11-29 08:17:30 -0700

    Wrote good behavor tests for HuffmanCode.

--- HW7/HuffmanCodeTest.java
+++ HW7/HuffmanCodeTest.java
@@ -1,4 +1,7 @@
-
+import java.util.PriorityQueue;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 import org.junit.After;
@@ -11,4 +14,40 @@ import org.junit.Test;
  * @version 11-28-12
  */
 public class HuffmanCodeTest {
+    // BEGIN Good Behavor Tests
+    /**
+     * Proves that analyse() gives a list of nodes with the correct letters
+     *  and frequencies.
+     */
+    @Test
+    public void testAnalyse() {
+        String input = "abcdead";
+        Map< Character, Integer > expected =
+            new HashMap< Character, Integer >();
+        expected.put( Character.valueOf( 'a' ), Integer.valueOf( 2 ) );
+        expected.put( Character.valueOf( 'b' ), Integer.valueOf( 1 ) );
+        expected.put( Character.valueOf( 'c' ), Integer.valueOf( 1 ) );
+        expected.put( Character.valueOf( 'd' ), Integer.valueOf( 2 ) );
+        expected.put( Character.valueOf( 'e' ), Integer.valueOf( 1 ) );
+        
+        Map< Character, Integer > actual = HuffmanCode.analyse( input );
+        assertEquals( expected.size(), actual.size() );
+        assertEquals( expected, actual );
+        
+        input = "cat fat head hat";
+        expected = new HashMap< Character, Integer >();
+        expected.put( Character.valueOf( 'c' ), Integer.valueOf( 1 ) );
+        expected.put( Character.valueOf( 'a' ), Integer.valueOf( 4 ) );
+        expected.put( Character.valueOf( 't' ), Integer.valueOf( 3 ) );
+        expected.put( Character.valueOf( ' ' ), Integer.valueOf( 3 ) );
+        expected.put( Character.valueOf( 'f' ), Integer.valueOf( 1 ) );
+        expected.put( Character.valueOf( 'h' ), Integer.valueOf( 2 ) );
+        expected.put( Character.valueOf( 'e' ), Integer.valueOf( 1 ) );
+        expected.put( Character.valueOf( 'd' ), Integer.valueOf( 1 ) );
+        
+        actual = HuffmanCode.analyse( input );
+        assertEquals( expected.size(), actual.size() );
+        assertEquals( expected, actual );
+    }
+    // END Good Behavor Tests
 }
