26d3ba801c47329406e34e2c8bea97a78e37a7f4
Josh Gillham <usajoshgillham@gmail.com>
2012-11-29 09:15:57 -0700

    Wrote a good behavior test for Exercises.strCount().

--- HW8/ExercisesTest.java
+++ HW8/ExercisesTest.java
@@ -6,10 +6,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * 
+ * Provides tests for Exercises.
  *
  * @author  Josh Gillham
  * @version 11-28-12
  */
 public class ExercisesTest {
+    // BEGIN Good Behavior Tests
+    /**
+     * Proves that strCount() can handle those tricky cases.
+     */
+    @Test
+    public void testStrCount_Tricky() {
+        assertEquals(1, (int)Exercises.strCount("aaa", "aa"));
+        assertEquals(2, (int)Exercises.strCount("aaaa", "aa"));
+        assertEquals(2, (int)Exercises.strCount("aaaaa", "aa"));
+        assertEquals(3, (int)Exercises.strCount("aaaaaa", "aa"));
+        assertEquals(3, (int)Exercises.strCount("aaaaaaa", "aa"));
+    }
+    // END Good Behavior Tests
 }
